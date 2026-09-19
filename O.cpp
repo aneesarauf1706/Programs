c08de3ee10e647d4c520d43816f9f84205376ec8
aneesarauf1706 <aneesarauf1706@gmail.com>
2026-09-19 13:14:54 +0500

    Add files via upload

---
+++ O.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+int main()
+{
+	for(int i=1; i<=6; i++)
+	{
+		for(int j=1; j<=6; j++)
+		if(i==1||i==6||j==1||j==6)
+		cout<<"*";
+		else
+		cout<<" ";
+		cout<<endl;
+	}
+}
